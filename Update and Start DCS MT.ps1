a0ab19a799e46e2378f3e33e71f006c4b01338f3
LOLBaltSS <58578360+LOLBaltSS@users.noreply.github.com>
2023-04-14 20:40:04 -0500

    Add files via upload

---
+++ Update and Start DCS MT.ps1
@@ -0,0 +1,7 @@
+﻿$OpenBetaPath = Get-ItemPropertyValue -Path 'HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\DCS World OpenBeta_is1\' -Name InstallLocation
+
+#Check for Updates
+Start-Process -FilePath "$OpenBetaPath\bin\DCS_updater.exe" -ArgumentList "update" -Wait
+
+#Start DCS after update completed
+Start-Process -FilePath "$OpenBetaPath\bin-mt\DCS.exe"
